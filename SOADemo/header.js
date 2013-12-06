46fc83b0ccecbcc5f49e9482258c5e26ee0e90a3
Rizwan Tejpar <rtejpar@gmail.com>
2013-12-06 12:03:33 -0500

    updated formatting for web interface assets

--- SOADemo/header.js
+++ SOADemo/header.js
@@ -6,38 +6,38 @@
  * @verified 07/22/07 (JSlint)
  * @browser IE6, IE7, Firefox 1.5, Firefox 2
  * @disclaimer
- *          Any references or links in this document to non-IBM Web sites are provided for convenience 
- *          only and do not in any manner serve as an endorsement of those non-IBM Web sites or their 
- *          owners. The materials at the non-IBM Web sites are not owned or licensed by IBM and use of 
- *          those non-IBM Web sites is at your own risk. IBM makes no representations, warranties, or 
- *          other commitments whatsoever about any non-IBM Web sites or third-party resources that may 
- *          be referenced, accessible from, or linked from this document. In addition, IBM is not a 
- *          party to or responsible for any transactions you may enter into with third parties, even if 
- *          you learn of such parties (or use a link to such parties) from this document. You are 
- *          responsible for compliance with any license terms or other obligations for use of the 
- *          non-IBM Web sites in respect of your use of those non-IBM Web sites. You acknowledge and 
- *          agree that IBM is not responsible for the availability of such external sites or resources, 
- *          and is not responsible or liable for any content, services, products, or other materials on 
- *          or available from those sites or resources. 
+ *          Any references or links in this document to non-IBM Web sites are provided for convenience
+ *          only and do not in any manner serve as an endorsement of those non-IBM Web sites or their
+ *          owners. The materials at the non-IBM Web sites are not owned or licensed by IBM and use of
+ *          those non-IBM Web sites is at your own risk. IBM makes no representations, warranties, or
+ *          other commitments whatsoever about any non-IBM Web sites or third-party resources that may
+ *          be referenced, accessible from, or linked from this document. In addition, IBM is not a
+ *          party to or responsible for any transactions you may enter into with third parties, even if
+ *          you learn of such parties (or use a link to such parties) from this document. You are
+ *          responsible for compliance with any license terms or other obligations for use of the
+ *          non-IBM Web sites in respect of your use of those non-IBM Web sites. You acknowledge and
+ *          agree that IBM is not responsible for the availability of such external sites or resources,
+ *          and is not responsible or liable for any content, services, products, or other materials on
+ *          or available from those sites or resources.
  * @disclaimer
- *          (c) Copyright IBM Corp. 2007 All rights reserved. 
- *			
- *          The following sample of source code ("Sample") is owned by International Business Machines 
- *          Corporation or one of its subsidiaries ("IBM") and is copyrighted and licensed, not sold. 
- *			
- *          The Sample is not part of any standard IBM product. You may use, copy, modify, and distribute 
- *          the Sample in any form without payment to IBM, for the purpose of assisting you in the 
- *          development of your applications.
- *			
- *          The Sample code is provided to you on an "AS IS" basis, without warranty of any kind. 
+ *          (c) Copyright IBM Corp. 2007 All rights reserved.
  *
- *          IBM HEREBY EXPRESSLY DISCLAIMS ALL WARRANTIES, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT 
- *          LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
- *           
- *          Some jurisdictions do not allow for the exclusion or limitation of implied warranties, so the 
- *          above limitations or exclusions may not apply to you. IBM shall not be liable for any damages 
- *          you suffer as a result of using, copying, modifying or distributing the Sample, even if IBM 
- *          has been advised of the possibility of such damages. 
+ *          The following sample of source code ("Sample") is owned by International Business Machines
+ *          Corporation or one of its subsidiaries ("IBM") and is copyrighted and licensed, not sold.
+ *
+ *          The Sample is not part of any standard IBM product. You may use, copy, modify, and distribute
+ *          the Sample in any form without payment to IBM, for the purpose of assisting you in the
+ *          development of your applications.
+ *
+ *          The Sample code is provided to you on an "AS IS" basis, without warranty of any kind.
+ *
+ *          IBM HEREBY EXPRESSLY DISCLAIMS ALL WARRANTIES, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT
+ *          LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *          Some jurisdictions do not allow for the exclusion or limitation of implied warranties, so the
+ *          above limitations or exclusions may not apply to you. IBM shall not be liable for any damages
+ *          you suffer as a result of using, copying, modifying or distributing the Sample, even if IBM
+ *          has been advised of the possibility of such damages.
  */
 
 /*
@@ -72,16 +72,16 @@ var list_update_id = null;
 
 /*
  * @name  hi_def_set
- * @desc  Stores an array of DOM Objects, each representing a single Health Definition from the 
- *		  Web Service Producer XML Document
+ * @desc  Stores an array of DOM Objects, each representing a single Health Definition from the
+ *      Web Service Producer XML Document
  * @scope global
  */
 var hi_def_set = null;
 
 /*
  * @name  hi_alert_set
- * @desc  Stores an array of DOM Objects, each representing a single Health Alert (Indicator) from the 
- *		  Web Service Producer XML Document
+ * @desc  Stores an array of DOM Objects, each representing a single Health Alert (Indicator) from the
+ *      Web Service Producer XML Document
  * @scope global
  */
 var hi_alert_set = null;
@@ -149,8 +149,8 @@ var hisTitleBar = null;
 
 /*
  * @name  diaTitleBar
- * @desc  Stores the TitleBar Widget Object used by the Dialog Widget on the Web Service Consumer GUI 
- *		  Console Page
+ * @desc  Stores the TitleBar Widget Object used by the Dialog Widget on the Web Service Consumer GUI
+ *      Console Page
  * @scope global
  */
 var diaTitleBar = null;
@@ -171,7 +171,7 @@ var detContentBar = null;
  */
 var hisContentBar = null;
 
-/* 
+/*
  * @name  diaContentBar
  * @desc  Stores the ContentBar Widget Object used by the Dialog Widget on the Web Service Consumer GUI
  *        Console Page
@@ -181,7 +181,7 @@ var diaContentBar = null;
 
 /*
  * @name  hisContentPane
- * @desc  Stores the history ContentPane Widget Object used by the Accordion Widget on the Web Service 
+ * @desc  Stores the history ContentPane Widget Object used by the Accordion Widget on the Web Service
  *        Consumer GUI Console Page
  * @scope global
  */
@@ -221,7 +221,7 @@ var memProgressBar = null;
 /*
  * @name  dskProgressBar
  * @desc  Stores the ProgressBar Widget Object used by the Web Service Consumer GUI Console Page
- *        to display DB2 memory consumption information 
+ *        to display DB2 memory consumption information
  * @scope global
  */
 var dskProgressBar = null;
